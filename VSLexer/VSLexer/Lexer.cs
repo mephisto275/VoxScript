fc36f5718ff50771d56b80542b14517c4c141a9c
Peternator7 <glotfelty.2@osu.edu>
2015-07-28 13:20:29 -0700

    Checking code in YOLO

--- VSLexer/VSLexer/Lexer.cs
+++ VSLexer/VSLexer/Lexer.cs
@@ -14,7 +14,8 @@ namespace VoxScript.Lexer
         identifierTok = 0,
         boolTok,
         stringTok,
-        numberTok,
+        longTok,
+        doubleTok,
         //Assignment
         setTok,
         assignTok,
@@ -53,6 +54,8 @@ namespace VoxScript.Lexer
         greaterThanOrEqualsTok,
         lessThanOrEqualsTok,
         exclusiveOrTok,
+        andTok,
+        orTok,
         //String Operations
         appendTok,
         //Lists
@@ -88,7 +91,7 @@ namespace VoxScript.Lexer
         Token Next { get; }
         Object Value { get; }
         String Identifier { get; }
-        void MoveNext();
+        Token MoveNext();
         void LoadScript(string input);
     }
 
@@ -106,22 +109,23 @@ namespace VoxScript.Lexer
         private StringReader lineReader = new StringReader("");
 
         private Dictionary<string, string> reservedWordToPhraseMap = new Dictionary<string, string>() {
-            {"equal","equal to"},
-            {"exit","exit loop"},
-            {"divided","divided by"},
-            {"greater","greater than|greater than or equal to"},
-            {"less","less than|less than or equal to"},
-            {"element","element from"},
-            {"type", "type of"},
-            {"new","list|function"},
-            {"exclusive","exclusive or"},
-            {"read","read line"}
+            {"equal","^equal +to"},
+            {"exit","^exit +loop"},
+            {"divided","^divided by"},
+            {"greater","^greater +than +or +equal +to|^greater +than"},
+            {"less","^less +than +or +equal +to|^less +than"},
+            {"element","^element +from"},
+            {"type", "^type +of"},
+            {"new","^new +list|^new +function"},
+            {"exclusive","^exclusive +or"},
+            {"read","^read +line"},
+            {"mod","^mod +by"}
         };
 
         private Dictionary<string, Token> phraseToTokenMap = new Dictionary<string, Token>() { 
             {"if",Token.ifTok},
             {"set",Token.setTok},
-            {"equal to",Token.assignTok},
+            {"^equal +to",Token.assignTok},
             {"then",Token.thenTok},
             {"otherwise",Token.otherwiseTok},
             {"loop",Token.loopTok},
@@ -134,30 +138,32 @@ namespace VoxScript.Lexer
             {"named",Token.namedTok},
             {"call",Token.callTok},
             {"next",Token.nextTok},
-            {"exit loop",Token.exitTok},
+            {"^exit +loop",Token.exitTok},
             {"return",Token.returnTok},
             {"plus", Token.plusTok},
             {"minus", Token.minusTok},
             {"times", Token.timesTok},
             {"multiplied",Token.timesTok},
-            {"divided by",Token.divideTok},
+            {"^divided +by",Token.divideTok},
             {"modulo",Token.moduloTok},
-            {"mod by",Token.moduloTok},
+            {"^mod +by",Token.moduloTok},
             {"negative",Token.negativeTok},
             {"negate",Token.negativeTok},
             {"true",Token.boolTok},
             {"false",Token.boolTok},
             {"is",Token.isTok},
             {"not",Token.notTok},
-            {"greater than",Token.greaterTok},
-            {"less than",Token.lessTok},
-            {"greater than or equal to",Token.greaterThanOrEqualsTok},
-            {"less than or equal to",Token.lessThanOrEqualsTok},
-            {"exclusive or",Token.exclusiveOrTok},
+            {"^greater +than",Token.greaterTok},
+            {"^less +than",Token.lessTok},
+            {"^greater +than +or +equal +to",Token.greaterThanOrEqualsTok},
+            {"^less +than +or +equal +to",Token.lessThanOrEqualsTok},
+            {"^exclusive +or",Token.exclusiveOrTok},
+            {"or",Token.orTok},
+            {"and",Token.andTok},
             {"append",Token.appendTok},
-            {"new function",Token.newFuncTok},
+            {"^new +function",Token.newFuncTok},
             {"with",Token.withTok},
-            {"new list",Token.newListTok},
+            {"^new +list",Token.newListTok},
             {"push",Token.pushTok},
             {"shift",Token.shiftTok},
             {"insert",Token.insertTok},
@@ -165,11 +171,11 @@ namespace VoxScript.Lexer
             {"pop",Token.popTok},
             {"unshift",Token.unshiftTok},
             {"length",Token.lengthTok},
-            {"element from",Token.elementTok},
+            {"^element +from",Token.elementTok},
             {"at",Token.atTok},
             {"print",Token.printTok},
-            {"read line",Token.readLineTok},
-            {"type of",Token.typeOfTok},
+            {"^read +line",Token.readLineTok},
+            {"^type +of",Token.typeOfTok},
             {"onto",Token.ontoTok}
         };
 
@@ -186,7 +192,7 @@ namespace VoxScript.Lexer
             MoveNext();
         }
 
-        public void MoveNext()
+        public Token MoveNext()
         {
             Current = Next;
             Identifier = nextIdentifier;
@@ -194,45 +200,47 @@ namespace VoxScript.Lexer
             if (Current == Token.eofTok || Current == Token.INVALID)
             {
                 Next = Token.INVALID;
-                return;
+                return Current;
             }
             if (currentLine == null)
             {
                 Next = Token.eofTok;
-                return;
+                return Current;
             }
+
+            HashSet<char> allowableCharactersAfterToken = new HashSet<char>() { ' ', ':', '.' };
 
             Char leadingChar = currentLine[currentPlace];
             if (Char.IsLetter(leadingChar))
             {
-                HashSet<char> allowableCharactersAfterIdentifier = new HashSet<char>() { ' ', ':', '.' };
-                Regex word = new Regex(@"\w+");
-                string accumulator = "";
+                
+                Regex word = new Regex(@"^[a-z]+",RegexOptions.IgnoreCase);
                 string remaining = currentLine.Substring(currentPlace);
                 Match match = word.Match(remaining);
 
-                bool isIdentifier;
+                bool isReserved;
                 //if the word is a keyword, or it's the first part of a key phrase, mark this as an identifier.
-                isIdentifier = !(phraseToTokenMap.ContainsKey(match.Value)
+                isReserved = (phraseToTokenMap.ContainsKey(match.Value)
                     || reservedWordToPhraseMap.ContainsKey(match.Value));
-                if (isIdentifier)
+                if (isReserved)
                 {
-                    //It's a single identifier.
+                    //It's a single reserved word.
                     if (phraseToTokenMap.ContainsKey(match.Value.ToLower()))
                     {
                         Next = phraseToTokenMap[match.Value.ToLower()];
+                        //Grab boolean literals if they occur.
                         if (match.Value.ToLower() == "true")
                         {
                             Next = Token.boolTok;
-                            Value = true;
+                            nextValue = true;
                         }
                         if (match.Value.ToLower() == "false")
                         {
                             Next = Token.boolTok;
-                            Value = false;
+                            nextValue = false;
                         }
                         currentPlace += match.Value.Length;
-                        if (!allowableCharactersAfterIdentifier.Contains(currentLine[currentPlace + 1]))
+                        if ((currentPlace + 1) < currentLine.Length && !allowableCharactersAfterToken.Contains(currentLine[currentPlace]))
                         {
                             Next = Token.INVALID;
                         }
@@ -243,13 +251,17 @@ namespace VoxScript.Lexer
                         bool matched = false;
                         foreach (string phrase in phrases)
                         {
-                            if (remaining.StartsWith(phrase))
+                            Regex phraseReg = new Regex(phrase, RegexOptions.IgnoreCase);
+                            Match phraseMatch = phraseReg.Match(remaining);
+                            if (phraseMatch.Success)
                             {
                                 Next = phraseToTokenMap[phrase];
+                                currentPlace += phraseMatch.Value.Length;
                                 matched = true;
+                                break;
                             }
                         }
-                        //If we couldn't match the whole phrase, abort.
+                        //If we can't match the whole phrase. It's an error.
                         if (!matched)
                         {
                             Next = Token.INVALID;
@@ -258,7 +270,64 @@ namespace VoxScript.Lexer
                 }
                 else
                 {
-                    accumulator += match.Value;
+                    //It's an identifier. Parse forward until we get to a reserved word.
+                    List<string> keywords = new List<string>();
+                    keywords.Add(match.Value);
+                    currentPlace += match.Value.Length;
+                    bool invalid = false;
+                    while (true)
+                    {
+                        if (currentPlace < currentLine.Length && !allowableCharactersAfterToken.Contains(currentLine[currentPlace]))
+                        {
+                            // The word isn't separated by a space, or followed by a : or .
+                            invalid = true;
+                            break;
+                        }
+                        //Trim out all the white space between words.
+                        while (currentPlace < currentLine.Length && currentLine[currentPlace] == ' ')
+                        {
+                            currentPlace++;
+                        }
+                        //If we got to the end of the line, break;
+                        if (currentPlace >= currentLine.Length)
+                        {
+                            break;
+                        }
+                        //If there is a non-alpha character, we're done.
+                        if (!Char.IsLetter(currentLine[currentPlace]))
+                        {
+                            break;
+                        }
+                        remaining = currentLine.Substring(currentPlace);
+                        match = word.Match(remaining);
+                        //If the next word is a reserved word, break.
+                        if ((phraseToTokenMap.ContainsKey(match.Value) || reservedWordToPhraseMap.ContainsKey(match.Value)))
+                        {
+                            //It's a reserved word. Break;
+                            break;
+                        }
+                        else
+                        {
+                            keywords.Add(match.Value.ToLower());
+                            currentPlace += match.Value.Length;
+                        }
+                    }
+                    if (!invalid)
+                    {
+                        StringBuilder identifier = new StringBuilder();
+                        keywords.Aggregate(identifier, (acc, keyword) =>
+                        {
+                            acc.Append("_");
+                            acc.Append(keyword);
+                            return acc;
+                        });
+                        Next = Token.identifierTok;
+                        nextIdentifier = identifier.ToString();
+                    }
+                    else
+                    {
+                        Next = Token.INVALID;
+                    }
                 }
             }
             else if (Char.IsDigit(leadingChar) || leadingChar == '-')
@@ -270,24 +339,32 @@ namespace VoxScript.Lexer
                 double possibleDouble;
                 if (long.TryParse(match.Value, out possibleLong))
                 {
-                    Next = Token.numberTok;
+                    Next = Token.longTok;
                     nextValue = possibleLong;
+                    currentPlace += match.Value.Length;
                 }
                 else if (double.TryParse(match.Value, out possibleDouble))
                 {
-                    Next = Token.numberTok;
+                    Next = Token.doubleTok;
                     nextValue = possibleDouble;
+                    currentPlace += match.Value.Length;
                 }
                 else
                 {
+                    Next = Token.INVALID;
+                }
+                if (currentPlace < currentLine.Length && !allowableCharactersAfterToken.Contains(currentLine[currentPlace]))
+                {
+                    //We don't have a space or anything following this. That's an error.
                     Next = Token.INVALID;
                 }
             }
             else if (leadingChar == '"')
             {
                 //If it's a quote, we need to grab everything to the next quote.
-                int startingPoint = currentPlace + 1;
-                while (currentPlace < currentLine.Length && currentLine[currentPlace] == '"')
+                currentPlace++;
+                int startingPoint = currentPlace;
+                while (currentPlace < currentLine.Length && currentLine[currentPlace] != '"')
                 {
                     currentPlace++;
                 }
@@ -300,39 +377,47 @@ namespace VoxScript.Lexer
                 {
                     nextValue = currentLine.Substring(startingPoint, currentPlace - startingPoint);
                     Next = Token.stringTok;
+                    currentPlace++;
+                    if (currentPlace < currentLine.Length && !allowableCharactersAfterToken.Contains(currentLine[currentPlace]))
+                    {
+                        //We don't have a space or anything following this. That's an error.
+                        Next = Token.INVALID;
+                    }
                 }
             }
             else if (leadingChar == '\t')
             {
                 //It's a tab. This should be the easiest case.
                 Next = Token.tabTok;
+                currentPlace++;
             }
             else if (leadingChar == ':')
             {
                 Next = Token.colonTok;
+                currentPlace++;
             }
             else if (leadingChar == '.')
             {
                 Next = Token.periodTok;
+                currentPlace++;
             }
             else
             {
                 Next = Token.INVALID;
             }
 
-            //Move it off of the last character.
-            currentPlace++;
             //Trim off white space until we get to the next token:
             while (currentPlace < currentLine.Length && currentLine[currentPlace] == ' ')
             {
                 currentPlace++;
             }
 
-            if (currentPlace == currentLine.Length)
+            if (currentPlace >= currentLine.Length)
             {
                 currentLine = lineReader.ReadLine();
             }
 
+            return Current;
         }
     }
 }
--- VSLexer/VSLexerTestApplication/Program.cs
+++ VSLexer/VSLexerTestApplication/Program.cs
@@ -3,13 +3,130 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VoxScript.Lexer;
 
 namespace VSLexerTestApplication
 {
     class Program
     {
+        private static Dictionary<Token,string> phraseToTokenMap = new Dictionary<Token,string>()
+        {
+            //Assignment
+            {Token.setTok, "set"},
+            {Token.assignTok, "equal to"},
+            //If...Then...Otherwise
+            {Token.ifTok, "if"},
+            {Token.thenTok, "then"},
+            {Token.otherwiseTok, "otherwise"},
+            //Loop Statements
+            {Token.loopTok, "loop"},
+            {Token.whileTok, "while"},
+            {Token.fromTok, "from"},
+            {Token.toTok, "to"},
+            {Token.byTok, "by"},
+            {Token.overTok, "over"},
+            {Token.intoTok, "into"},
+            {Token.namedTok, "named"},
+            {Token.nextTok, "next"},
+            {Token.exitTok, "exit"},
+            //Function calls
+            {Token.callTok, "call"},
+            {Token.returnTok, "return"},
+            {Token.newFuncTok, "new function"},
+            {Token.withTok, "with"},
+            //Numeric Operations
+            {Token.plusTok, "plus"},
+            {Token.minusTok, "minus"},
+            {Token.timesTok, "times"},
+            {Token.divideTok, "divided by"},
+            {Token.moduloTok, "modulo"},
+            {Token.negativeTok, "negative"},
+            //Boolean Operations
+            {Token.isTok, "is" },
+            {Token.notTok, "not"},
+            {Token.greaterTok, "greater than"},
+            {Token.lessTok, "less than"},
+            {Token.greaterThanOrEqualsTok, "greater than or equal to"},
+            {Token.lessThanOrEqualsTok, "less than or equal to"},
+            {Token.exclusiveOrTok, "exclusive or"},
+            {Token.andTok, "and"},
+            {Token.orTok, "or"},
+            //String Operations
+            {Token.appendTok, "append"},
+            //Lists
+            {Token.newListTok, "new list"},
+            {Token.pushTok, "push"},
+            {Token.popTok, "pop"},
+            {Token.ontoTok, "onto"},
+            {Token.shiftTok, "shift"},
+            {Token.unshiftTok, "unshift"},
+            {Token.insertTok, "insert into"},
+            {Token.removeTok, "remove from"},
+            {Token.lengthTok, "length of"},
+            {Token.elementTok, "element from"},
+            {Token.atTok, "at"},
+            //Type
+            {Token.typeOfTok, "type of"},
+            //IO
+            {Token.printTok, "print"},
+            {Token.readLineTok, "read line"},
+            //Other control flow
+            {Token.colonTok, ": [New Line]"},
+            {Token.tabTok, "Tab"},
+            {Token.eofTok, "EOF"},
+            {Token.periodTok, ". [New Line]"},
+            //Temp values.
+            {Token.UNINITIALIZED, "UNINITIALIZED"},
+            {Token.INVALID, "INVALID"}
+        };
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Enter a vox script line.");
+                string input = Console.ReadLine();
+                Lexer lexer = new Lexer();
+                lexer.LoadScript(input);
+
+                while ((lexer.MoveNext() != Token.eofTok) && (lexer.Current != Token.INVALID))
+                {
+                    Token current = lexer.Current;
+                    Console.Write(" [");
+                    if (current == Token.boolTok)
+                    {
+                        Console.Write((bool)lexer.Value);
+                    }
+                    else if (current == Token.stringTok)
+                    {
+                        Console.Write("\"{0}\"",(string)lexer.Value);
+                    }
+                    else if (current == Token.longTok)
+                    {
+                        Console.Write("{0}", (long)lexer.Value);
+                    }
+                    else if (current == Token.doubleTok)
+                    {
+                        Console.Write("{0}", (double)lexer.Value);
+                    }
+                    else if (current == Token.identifierTok)
+                    {
+                        Console.Write(lexer.Identifier);
+                    }
+                    else if (current == Token.periodTok || current == Token.colonTok)
+                    {
+                        Console.WriteLine(phraseToTokenMap[current] + "]");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Write(phraseToTokenMap[current]);
+                    }
+                    Console.Write("]");
+                }
+                Console.WriteLine();
+                Console.WriteLine("finished");
+            }
         }
     }
 }
